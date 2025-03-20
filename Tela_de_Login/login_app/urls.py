2efee95f91afa571504d35b4aa84cb37f5951ac0
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-03-20 13:44:32 -0300

    excluir
    Fazer tudo de novo

--- Tela_de_Login/login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import views as auth_views  
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.cadastro, name='cadastro'),  
-    path('login/', auth_views.LoginView.as_view(template_name='Cadastro/login.html', next_page='/home/'), name='login'),  
-    path('home/', views.home, name='home'),  
-]
